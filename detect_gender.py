53650bef47e43739fe53c5d6395f5bf85d19907d
Dtinubu <73240396+Dtinubu@users.noreply.github.com>
2021-03-17 14:50:55 -0400

    Update detect_gender.py

--- detect_gender.py
+++ detect_gender.py
@@ -15,72 +15,87 @@ import cvlib as cv
 ap = argparse.ArgumentParser()
 ap.add_argument("-i", "--image", required=True,
 	help="path to input image")
+ap.add_argument("-t", "--text_file", required=True,
+	help="path to input image")
+ap.add_argument("-r", "--race", required=True,
+	help="path to input image")
 args = ap.parse_args()
+
 
 # download pre-trained model file (one-time download)
 dwnld_link = "https://github.com/arunponnusamy/cvlib/releases/download/v0.2.0/gender_detection.model"
 model_path = get_file("gender_detection.model", dwnld_link,
                      cache_subdir="pre-trained", cache_dir=os.getcwd())
 
-# read input image
-image = cv2.imread(args.image)
+ images_root = os.path.join('/cmlscratch','dtinubu','datasets','RFW','Balancedface','race_per_7000', race)
+    names = os.listdir(images_root)
+    if len(names) == 0:
+        raise RuntimeError('Empty dataset')
 
-if image is None:
-    print("Could not read input image")
-    exit()
+     for klass, name in enumerate(names):
+	image_path = os.path.join(images_root, name, image)
+        images_of_person = os.listdir(os.path.join(images_root, name))
+	
+	# read input image
+	image = cv2.imread(args.image)
 
-# load pre-trained model
-model = load_model(model_path)
+	if image is None:
+	    print("Could not read input image")
+	    exit()
 
-# detect faces in the image
-face, confidence = cv.detect_face(image)
+	# load pre-trained model
+	model = load_model(model_path)
 
-classes = ['man','woman']
+	# detect faces in the image
+	face, confidence = cv.detect_face(image)
 
-# loop through detected faces
-for idx, f in enumerate(face):
+	classes = ['man','woman']
 
-     # get corner points of face rectangle       
-    (startX, startY) = f[0], f[1]
-    (endX, endY) = f[2], f[3]
+	# loop through detected faces
+	for idx, f in enumerate(face):
 
-    # draw rectangle over face
-    cv2.rectangle(image, (startX,startY), (endX,endY), (0,255,0), 2)
+	     # get corner points of face rectangle       
+	    (startX, startY) = f[0], f[1]
+	    (endX, endY) = f[2], f[3]
 
-    # crop the detected face region
-    face_crop = np.copy(image[startY:endY,startX:endX])
+	    # draw rectangle over face
+	    cv2.rectangle(image, (startX,startY), (endX,endY), (0,255,0), 2)
 
-    # preprocessing for gender detection model
-    face_crop = cv2.resize(face_crop, (96,96))
-    face_crop = face_crop.astype("float") / 255.0
-    face_crop = img_to_array(face_crop)
-    face_crop = np.expand_dims(face_crop, axis=0)
+	    # crop the detected face region
+	    face_crop = np.copy(image[startY:endY,startX:endX])
 
-    # apply gender detection on face
-    conf = model.predict(face_crop)[0]
-    print(conf)
-    print(classes)
+	    # preprocessing for gender detection model
+	    face_crop = cv2.resize(face_crop, (96,96))
+	    face_crop = face_crop.astype("float") / 255.0
+	    face_crop = img_to_array(face_crop)
+	    face_crop = np.expand_dims(face_crop, axis=0)
 
-    # get label with max accuracy
-    idx = np.argmax(conf)
-    label = classes[idx]
+	    # apply gender detection on face
+	    conf = model.predict(face_crop)[0]
+	    print(conf)
+	    print(classes)
 
-    label = "{}: {:.2f}%".format(label, conf[idx] * 100)
+	    # get label with max accuracy
+	    idx = np.argmax(conf)
+	    label = classes[idx]
 
-    Y = startY - 10 if startY - 10 > 10 else startY + 10
+	    label = "{}: {:.2f}%".format(label, conf[idx] * 100)
 
-    # write label and confidence above face rectangle
-    cv2.putText(image, label, (startX, Y),  cv2.FONT_HERSHEY_SIMPLEX,
-                0.7, (0, 255, 0), 2)
+	    Y = startY - 10 if startY - 10 > 10 else startY + 10
 
-# display output
-cv2.imshow("gender detection", image)
+	    # write label and confidence above face rectangle
+	    cv2.putText(image, label, (startX, Y),  cv2.FONT_HERSHEY_SIMPLEX,
+			0.7, (0, 255, 0), 2)
+		
+	
+	
+	
 
-# press any key to close window           
-cv2.waitKey()
+	# press any key to close window           
+	cv2.waitKey()
 
-# save output
-cv2.imwrite("gender_detection.jpg", image)
+	# save output
+	cv2.imwrite("gender_detection.jpg", image)
 
 # release resources
 cv2.destroyAllWindows()
