daba050cfe27eac27c207997f9f8fb43f2e8a173
Dtinubu <73240396+Dtinubu@users.noreply.github.com>
2021-03-24 12:46:02 -0400

    Update detect_gender.py

--- detect_gender.py
+++ detect_gender.py
@@ -62,18 +62,15 @@ for klass, name in enumerate(names):
                        face_crop = np.expand_dims(face_crop, axis=0)
 
 			    # apply gender detection on face
-		       conf = model.predict(face_crop)[0]
-		       print(conf)
-		       print(classes)
+                       conf = model.predict(face_crop)[0]
+                       print(conf)
+                       print(classes)
 
-			    # get label with max accuracy
-		       idx = np.argmax(conf)
-		       label = classes[idx]
-
-		       label = "{}: {:.2f}%".format(label, conf[idx] * 100)
-
+			    # get label with max acc
+                       idx = np.argmax(conf)
+                       label = classes[idx]
+                       label = "{}: {:.2f}%".format(label, conf[idx] * 100)
 		       Y = startY - 10 if startY - 10 > 10 else startY + 10
-
 			     # write label and confidence above face rectangle
 		       f.write(label + "," + name + "," + image_path )
 
