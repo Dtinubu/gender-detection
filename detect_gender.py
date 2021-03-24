39e3c9239b34777da22cbfa4bc786dda28227a2a
Dtinubu <73240396+Dtinubu@users.noreply.github.com>
2021-03-24 09:32:21 -0400

    Update detect_gender.py

--- detect_gender.py
+++ detect_gender.py
@@ -50,37 +50,35 @@ for klass, name in enumerate(names):
 		  for idx, f in enumerate(face):
 
 		     # get corner points of face rectangle       
-		  (startX, startY) = f[0], f[1]
-		  (endX, endY) = f[2], f[3]
+		     (startX, startY) = f[0], f[1]
+		     (endX, endY) = f[2], f[3]
 
 		    # draw rectangle over face
-		  cv2.rectangle(image, (startX,startY), (endX,endY), (0,255,0), 2)
+		     cv2.rectangle(image, (startX,startY), (endX,endY), (0,255,0), 2)
 
 		    # crop the detected face region
-		  face_crop = np.copy(image[startY:endY,startX:endX])
+		     face_crop = np.copy(image[startY:endY,startX:endX])
 
 		    # preprocessing for gender detection model
-		  face_crop = cv2.resize(face_crop, (96,96))
-		  face_crop = face_crop.astype("float") / 255.0
-		  face_crop = img_to_array(face_crop)
-		  face_crop = np.expand_dims(face_crop, axis=0)
+		    face_crop = cv2.resize(face_crop, (96,96))
+		    face_crop = face_crop.astype("float") / 255.0
+		    face_crop = img_to_array(face_crop)
+		    face_crop = np.expand_dims(face_crop, axis=0)
 
 		    # apply gender detection on face
-		  conf = model.predict(face_crop)[0]
-		  print(conf)
-		  print(classes)
+		    conf = model.predict(face_crop)[0]
+		    print(conf)
+		    print(classes)
 
 		    # get label with max accuracy
-		  idx = np.argmax(conf)
-		  label = classes[idx]
+		    idx = np.argmax(conf)
+		    label = classes[idx]
 
-		  label = "{}: {:.2f}%".format(label, conf[idx] * 100)
+		    label = "{}: {:.2f}%".format(label, conf[idx] * 100)
 
-		  Y = startY - 10 if startY - 10 > 10 else startY + 10
+		    Y = startY - 10 if startY - 10 > 10 else startY + 10
 
 		    # write label and confidence above face rectangle
-		  cv2.putText(image, label, (startX, Y),  cv2.FONT_HERSHEY_SIMPLEX,
-				0.7, (0, 255, 0), 2)
 		  f.write(label + "," + name + "," + image_path )
 
 
