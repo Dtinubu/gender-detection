6f6fcfaa7993856bcb223efd2536c9c735a3a353
Dtinubu <73240396+Dtinubu@users.noreply.github.com>
2021-03-24 09:29:42 -0400

    Update detect_gender.py

--- detect_gender.py
+++ detect_gender.py
@@ -29,63 +29,60 @@ model_path = get_file("gender_detection.model", dwnld_link,
 f = open(args.text_file+".txt","w+")
 images_root = os.path.join('/cmlscratch','dtinubu','datasets','RFW','Balancedface','race_per_7000', race)
 names = os.listdir(images_root)
-if len(names) == 0:
-	raise RuntimeError('Empty dataset')
-for klass, name in enumerate(names):
-          image_path = os.path.join(images_root, name, ex)
-          images_of_person = os.listdir(os.path.join(images_root, name))
-	# read input image
-	  image = cv2.imread(image_path)
+	for klass, name in enumerate(names):
+		  image_path = os.path.join(images_root, name, ex)
+		  images_of_person = os.listdir(os.path.join(images_root, name))
+		# read input image
+		  image = cv2.imread(image_path)
+		  if image is None:
+		    print("Could not read input image")
+			exit()
 
-	  if image is None:
-	    print("Could not read input image")
-	    	exit()
+		# load pre-trained model
+		  model = load_model(model_path)
 
-	# load pre-trained model
-          model = load_model(model_path)
+		# detect faces in the image
+		  face, confidence = cv.detect_face(image)
 
-	# detect faces in the image
-	  face, confidence = cv.detect_face(image)
+		  classes = ['man','woman']
 
-	  classes = ['man','woman']
+		# loop through detected faces
+		  for idx, f in enumerate(face):
 
-	# loop through detected faces
-	  for idx, f in enumerate(face):
+		     # get corner points of face rectangle       
+		  (startX, startY) = f[0], f[1]
+		  (endX, endY) = f[2], f[3]
 
-	     # get corner points of face rectangle       
-	  (startX, startY) = f[0], f[1]
-	  (endX, endY) = f[2], f[3]
+		    # draw rectangle over face
+		  cv2.rectangle(image, (startX,startY), (endX,endY), (0,255,0), 2)
 
-	    # draw rectangle over face
-	  cv2.rectangle(image, (startX,startY), (endX,endY), (0,255,0), 2)
+		    # crop the detected face region
+		  face_crop = np.copy(image[startY:endY,startX:endX])
 
-	    # crop the detected face region
-	  face_crop = np.copy(image[startY:endY,startX:endX])
+		    # preprocessing for gender detection model
+		  face_crop = cv2.resize(face_crop, (96,96))
+		  face_crop = face_crop.astype("float") / 255.0
+		  face_crop = img_to_array(face_crop)
+		  face_crop = np.expand_dims(face_crop, axis=0)
 
-	    # preprocessing for gender detection model
-	  face_crop = cv2.resize(face_crop, (96,96))
-	  face_crop = face_crop.astype("float") / 255.0
-	  face_crop = img_to_array(face_crop)
-	  face_crop = np.expand_dims(face_crop, axis=0)
+		    # apply gender detection on face
+		  conf = model.predict(face_crop)[0]
+		  print(conf)
+		  print(classes)
 
-	    # apply gender detection on face
-	  conf = model.predict(face_crop)[0]
-	  print(conf)
-	  print(classes)
+		    # get label with max accuracy
+		  idx = np.argmax(conf)
+		  label = classes[idx]
 
-	    # get label with max accuracy
-	  idx = np.argmax(conf)
-	  label = classes[idx]
+		  label = "{}: {:.2f}%".format(label, conf[idx] * 100)
 
-	  label = "{}: {:.2f}%".format(label, conf[idx] * 100)
+		  Y = startY - 10 if startY - 10 > 10 else startY + 10
 
-	  Y = startY - 10 if startY - 10 > 10 else startY + 10
+		    # write label and confidence above face rectangle
+		  cv2.putText(image, label, (startX, Y),  cv2.FONT_HERSHEY_SIMPLEX,
+				0.7, (0, 255, 0), 2)
+		  f.write(label + "," + name + "," + image_path )
 
-	    # write label and confidence above face rectangle
-	  cv2.putText(image, label, (startX, Y),  cv2.FONT_HERSHEY_SIMPLEX,
-			0.7, (0, 255, 0), 2)
-	  f.write(label + "," + name + "," + image_path )
-		
 
 	# press any key to close window           
 	cv2.waitKey()
