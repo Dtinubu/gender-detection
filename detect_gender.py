9779c1cc281c08da9eabf0bc3e25c27107dfef4c
Dtinubu <73240396+Dtinubu@users.noreply.github.com>
2021-03-24 12:37:11 -0400

    Update detect_gender.py

--- detect_gender.py
+++ detect_gender.py
@@ -46,9 +46,8 @@ for klass, name in enumerate(names):
 		    classes = ['man','woman']
 	            # loop through detected faces
 		    for idx, f in enumerate(face):
-	            # get corner points of face rectangle
-		    (startX, startY) = f[0], f[1]
-		    (endX, endY) = f[2], f[3]
+	            	(startX, startY) = f[0], f[1]
+		    	(endX, endY) = f[2], f[3]
 		     # draw rectangle over face
 			     cv2.rectangle(image, (startX,startY), (endX,endY), (0,255,0), 2)
 
