88098478a08aac349a5b7e3c703f49edafddbd6a
Dtinubu <73240396+Dtinubu@users.noreply.github.com>
2021-04-07 14:55:57 -0400

    Update detect_gender.py

--- detect_gender.py
+++ detect_gender.py
@@ -37,32 +37,9 @@ for klass, name in enumerate(names):
                       print("Could not read input image")
                       exit()
 			
-		    # load pre-trained model
-                    model = load_model(model_path)
-			# detect faces in the image
-                    face, confidence = cv.detect_face(image)
-                    classes = ['man','woman']
-	            # loop through detected faces
-                    for idx, f in enumerate(face):
-                        (startX, startY) = f[0], f[1]
-                        (endX, endY) = f[2], f[3]
-		     # draw rectangle over face
-                        cv2.rectangle(image, (startX,startY), (endX,endY), (0,255,0), 2)
-
-			    # crop the detected face region
-                        face_crop = np.copy(image[startY:endY,startX:endX])
-
-			    # preprocessing for gender detection model
-                        face_crop = cv2.resize(face_crop, (96,96))
-                        face_crop = face_crop.astype("float") / 255.0
-                        face_crop = img_to_array(face_crop)
-                        face_crop = np.expand_dims(face_crop, axis=0)
-
-			    # apply gender detection on face
-                        conf = model.predict(face_crop)[0]
-                        print(conf)
-                        print(classes)
-
+		     faces, confidences = cv.detect_face(image) 
+		     label, confidence = cv.detect_gender(face)
+		
 			    # get label with max acc
                         idx = np.argmax(conf)
                         label = classes[idx]
