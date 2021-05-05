449b0f383fa0fdf16aa3d43f8fbdd75fa3226d89
Dtinubu <73240396+Dtinubu@users.noreply.github.com>
2021-05-05 13:37:05 -0400

    Update detect_gender.py

--- detect_gender.py
+++ detect_gender.py
@@ -31,17 +31,20 @@ for klass, name in enumerate(names):
 		  images_of_person = os.listdir(os.path.join(images_root, name))
 		  for  i in range (len(images_of_person)):
                     path =  os.path.join(images_root, name, images_of_person[i])
+                  man = 0
+                  woman = 0
 		    # read input image
                     image = cv2.imread(path)
                     if image is None:
                       print("Could not read input image")
                       exit()
-                      faces, confidences = cv.detect_face(image) 
-                      label, confidence = cv.detect_gender(face)
-                      f = open(args.text_file+".txt","a")
-                      f.write(label + "," + name + "," + path + "\n")
-                      print(label + "," + name + "," + path + "\n")
-                      f.close()
+                    faces, confidences = cv.detect_face(image) 
+                    label, confidence = cv.detect_gender(face)
+                    f = open(args.text_file+".txt","a")
+                    f.write(label + "," + name + "," + path + "\n")
+                    f.close()
+		    print(label + "," + name + "," + path + "\n")
+	
 
 
 # press any key to close window
